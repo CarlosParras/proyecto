275fa042a4fecca65480196c8631b16e37798dc6
enrique <enrique@VAIO>
2016-05-18 13:00:19 -0500

    esta es la primera version del proyecto que se va a desarrollar

---
+++ Reserva/src/reserva/Reserva.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reserva;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Parras
+ */
+public class Reserva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Sis13-A");
+    }
+    
+}
